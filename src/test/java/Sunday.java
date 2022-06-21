076c3aaef98cffbef0a5d11c849b7588243cc02e
Prakashmekkavan <prakash@gmail.com>
2022-06-21 15:28:31 +0530

    new project

---
+++ src/test/java/Sunday.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class Sunday {
+
+	
+	WebDriverManager.chromedriver().setup();
+WebDriver dr= new ChromeDriver();	
+	 
+
+} 
